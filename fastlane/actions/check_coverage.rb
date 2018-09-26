be51272ad898d97f861c6b065b4c0931c9c4e947
Jimmy Yezeguelian <jimmy.yezeguelian@rakuten.com>
2018-09-26 17:23:31 +0200

    Changes

--- fastlane/actions/check_coverage.rb
+++ fastlane/actions/check_coverage.rb
@@ -28,7 +28,7 @@ module Fastlane
     	basename_option = ""
 	    if basenames.length > 1
 	    	basenames.each_with_index {|val, index|
-	    		if index == 0
+	    		if index == 1
         			basename_option = "--binary-basename #{val} "
         		else
 					basename_option = "--binary-basename #{val}"
